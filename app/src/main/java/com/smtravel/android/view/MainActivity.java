63423784e65e41787b20af5b31890fef57f30add
emincankarasoy <emincannkarasoy@gmail.com>
2021-10-03 13:47:01 +0300

    Smart Travel Android

--- app/src/main/java/com/smtravel/android/view/MainActivity.java
+++ app/src/main/java/com/smtravel/android/view/MainActivity.java
@@ -1,6 +1,10 @@
 package com.smtravel.android.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.smtravel.android.R;
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }, 3000);
     }
 }
